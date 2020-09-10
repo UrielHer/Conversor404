e6ce7b5aa5b3dada0f3089df5cd30013ab1e3c84
UrielHer <67615601+UrielHer@users.noreply.github.com>
2020-09-10 11:54:58 -0500

    Add files via upload

---
+++ ConversorUnidad/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Conversor</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<section>
+        <hr><center><h1>Conversor Metros</h1></center></hr>
+        <form action="ControladorConvertidor.php" method="post">
+            <label>Dato</label>
+            <input type="text" name="cantidad" required /><br>
+            <input type="radio" name="unidad" value="1">Centímetros<br>
+            <input type="radio" name="unidad" value="2">Milímetros<br>
+            <input type="radio" name="unidad" value="3">Kilometros<br>
+            <input type="submit" name="btnconvertir" value="Convertir">
+        </form>
+
+        <!--<?php
+        	if(isset($mensaje) and $mensaje != '')
+        	{
+        		echo $mensaje;
+        	}
+        	else{
+        	$mensaje = '';
+        	echo $mensaje;
+        }
+        ?>-->
+
+    </section>
+
+    <section>
+        <hr><center><h1>Conversor Litros</h1></center></hr>
+        <form action="ControladorConvertidor.php" method="post">
+            <label>Dato</label>
+            <input type="text" name="cantidad" required /><br>
+            <input type="radio" name="unidad" value="4">Decilitros<br>
+            <input type="radio" name="unidad" value="5">Mililirtos<br>
+            <input type="submit" name="btnconvertir" value="Convertir">
+        </form>
+
+        <!--<?php
+        	if(isset($mensaje) and $mensaje != '')
+        	{
+        		echo $mensaje;
+        	}
+        	else{
+        	$mensaje = '';
+        	echo $mensaje;
+        }
+        ?>-->
+
+    </section>
+
+    <section>
+        <hr><center><h1>Conversor Kilogramos</h1></center></hr>
+        <form action="ControladorConvertidor.php" method="post">
+            <label>Dato</label>
+            <input type="text" name="cantidad" required /><br>
+            <input type="radio" name="unidad" value="6">Gramos<br>
+            <input type="radio" name="unidad" value="7">Miligramos<br>
+            <input type="submit" name="btnconvertir" value="Convertir">
+        </form>
+
+        <!--<?php
+        	if(isset($mensaje) and $mensaje != '')
+        	{
+        		echo $mensaje;
+        	}
+        	else{
+        	$mensaje = '';
+        	echo $mensaje;
+        }
+        ?>-->
+
+    </section>
+
+    <hr><center><h1>Resultado</h1></center></hr>
+    <?php
+        	if(isset($mensaje) and $mensaje != '')
+        	{
+        		echo $mensaje;
+        	}
+        	else{
+        	$mensaje = '';
+        	echo $mensaje;
+        }
+        ?>
+</body>
+</html>
